70b7b3935d718c1e9457734592d8fd19ac622da3
rishabh <rishabhh2000@gmail.com>
2022-09-15 14:09:22 +0530

    Creating the Header Component and Logo

---
+++ src/common/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from "../../assets/logo.svg"
+import "./Header.css"
+const Header = () => {
+  return (
+    <div className='Header'>
+      Header
+      <img src={logo} alt="logo"/>
+    </div>
+  )
+}
+
+export default Header
